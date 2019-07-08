afdd021c796b1217394753f35bcb2c4db290cfed
Ahmeda1708 <ahmed.ayman1708@gmail.com>
2019-07-08 18:59:41 +0200

    Example 1 Creation

---
+++ Lecture 5/Double Linked List/Example1/cmake-build-debug/CMakeFiles/Example1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Example1.dir/main.c.o"
+  "Example1.pdb"
+  "Example1.exe"
+  "libExample1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Example1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
